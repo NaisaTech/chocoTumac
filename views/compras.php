088e16b0be028aeaf149d6654aa506be6a45611e
NATH NATH <nathaliamejia155@gmail.com>
2026-05-11 02:12:20 -0500

    fix(security: agrega subresource integrity a cdn de bpptstrap a compras.php

--- views/compras.php
+++ views/compras.php
@@ -51,7 +51,7 @@ $rol = $_SESSION['user']['rol_id'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Compras – Chocolate Tumaco</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="/chocoTumac/public/css/styles.css">
 </head>
 <body>
@@ -264,7 +264,7 @@ $rol = $_SESSION['user']['rol_id'];
     </div>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVzdl1" crossorigin="anonymous"></script>
 <script src="/chocoTumac/public/js/app.js"></script>
 <script src="/chocoTumac/public/js/compras.js"></script>
 </body>
